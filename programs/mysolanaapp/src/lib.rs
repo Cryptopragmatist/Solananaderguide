f1b1d10fd7b77d5da95f829ba0b12e83b5d3d4e3
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-23 19:31:42 +0800

    constraints and instructions explanation

--- programs/mysolanaapp/src/lib.rs
+++ programs/mysolanaapp/src/lib.rs
@@ -5,13 +5,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod mysolanaapp {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult { //initialize: when invoked exits the program successfully if arguments are met
+   /* pub fn initialize(ctx: Context<Initialize>) -> ProgramResult { //initialize: when invoked exits the program successfully if arguments are met
         Ok(())
-    } 
+    } */
     /*two functions are the RPC request handlers that we will be able to call from a client
      application to interact with the program */
 
-    pub fn create(ctx: Context<Initialize>) -> ProgramResult {  //
+    pub fn create(ctx: Context<Create>) -> ProgramResult {  // to call this fn, you need complete the Create Context which is a struct
         let base_account = &mut ctx.accounts.base_account;
         base_account.count = 0;
         Ok(())
@@ -28,9 +28,10 @@ pub mod mysolanaapp {
 //pub struct Initialize {}
 
 //Transaction instructions
-pub struct Create<'info> {
+pub struct Create<'info> { //the Create struct will need the following three parameters to execute
+    //#[account(...)] attributes define constraints and instructions if not met, will not execute
     #[account(init, payer = user, space = 16 + 16)]
-    pub base_account: Account<'info, BaseAccount>,
+    pub base_account: Account<'info, BaseAccount>, //any client with a proper base account can call the RPC methods
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program <'info, System>,
